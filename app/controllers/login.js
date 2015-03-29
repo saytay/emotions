13c36832681fa3d2d11d360d42a6e25d808eecd4
Gordon Cassie <gordoncassie@gmail.com>
2015-03-29 15:21:25 -0400

    Implement dropbox link.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,28 +1,52 @@
+// put this up here so we can access `client` everywhere in our controller
+var client = new Dropbox.Client({key: 'n4iqdi06xsx7bef'}); // need to use quotes because the key is a string
+
 export default Ember.ObjectController.extend({
   // initial value
   isClicked: false,
 
+  tasks: [],
+
   actions: {
     Login: function() {
       this.set('isClicked', true);
-  //link accounts
 
-
-      var client = new Dropbox.Client({key: n4iqdi06xsx7bef});//be sure to replace APP_KEY with the real value for your app}}
-
-      // Try to finish OAuth authorization.
-      client.authenticate({interactive: false}, function (error) {
+      client.authenticate(function (error) {
           if (error) {
               alert('Authentication error: ' + error);
           }
       });
+    },
 
-      if (client.isAuthenticated()) {
-          // Client is authenticated. Display UI.
-    }
+    addMilk: function(){
+      var datastoreManager = client.getDatastoreManager();
+      datastoreManager.openDefaultDatastore(function (error, datastore) {
+          if (error) {
+              alert('Error opening default datastore: ' + error);
+          }
 
-},
+          var taskTable = datastore.getTable('tasks');
+          var firstTask = taskTable.insert({
+            taskname: 'Buy milk',
+            completed: false,
+            created: new Date()
+          });
+      });
+    },
 
+    updateTasks: function(){
+      var datastoreManager = client.getDatastoreManager();
+      var self = this;
+      datastoreManager.openDefaultDatastore(function (error, datastore) {
+          if (error) {
+              alert('Error opening default datastore: ' + error);
+          }
+
+          var taskTable = datastore.getTable('tasks');
+          var tasks = taskTable.query();
+          self.set('tasks', tasks);
+      });
+    },
 
     Nothing: function() {
       this.set('isClicked',false);
